3bb1110ef29a90f4a8548c6296b24c36d42a318e
sogbdn <sonia.barretdenazaris@gmail.com>
2019-02-22 21:00:12 +0000

    HTTP basic auth added for admin pages

--- app/controllers/admin/categories_controllers.rb
+++ app/controllers/admin/categories_controllers.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+  
   def index
     @categories = category.order(id: :desc).all
   end
