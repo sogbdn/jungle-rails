0f1c6f89d93e70982c470e042ef474cd5df4c4c9
sogbdn <sonia.barretdenazaris@gmail.com>
2019-02-26 20:57:15 +0000

    Added Feature Spec on Product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end  
+
+  scenario "They see all products" do # like it...do block 
+    # ACT
+    visit root_path
+     # DEBUG
+    save_and_open_screenshot
+     # VERIFY
+    expect(page).to have_css 'article.product', count: 10
+
+  end
+
+  scenario "They see one product" do
+    visit root_path
+
+    click_link "#{@category.products[0].name}"
+    expect(page).to have_current_path(product_path(@category.products[0]))
+  end
+end
